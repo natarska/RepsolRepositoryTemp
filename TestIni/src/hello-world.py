b5e9b49bbbf5486bb7d054356c9cc0c996852a8d
natarska <natarska@hotmail.com>
2018-12-10 15:51:14 +0100

    Mi comit propio

---
+++ TestIni/src/hello-world.py
@@ -0,0 +1,25 @@
+import threading
+
+
+# threads = list()
+
+# def worker(count):
+#     print ("Este es el %s trabajo que hago hoy para Genbeta Dev" % count)
+#     return
+
+# def startThreads():
+#     for i in range(3):
+#         t = threading.Thread(target=worker, args=(i,))
+#         threads.append(t)
+#         t.start()
+#         return 
+
+class Tester:
+    def __init__ (self, url, x, y):
+        self.url:'hola'
+        self.x:x
+        self.y:'caramba'
+
+objeto = Tester('adios','pasante','caray')
+print('Este es mi objeto '+ objeto.x)
+
